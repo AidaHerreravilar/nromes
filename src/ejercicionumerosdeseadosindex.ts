10d4b378a59bf38b1ec35da9308639c5b063d3a8
AidaHerreravilar <92064121+AidaHerreravilar@users.noreply.github.com>
2021-11-06 10:01:13 -0300

    Initial commit

---
+++ src/ejercicionumerosdeseadosindex.ts
@@ -0,0 +1,7 @@
+let numerodeseado: number[] = new Array(5);
+let nro : number; indice : number;
+
+for (indice =0; indice <5; indice++){
+  nro = number(prompt("indique el numero que desea incorporar en la posicion$(indice}: ));
+  
+}
---
+++ src/nromesindex.ts
@@ -0,0 +1,21 @@
+let nromes: number [] = new Array (7);
+
+nromes[0] = 0;
+nromes[1] = 0;
+nromes[2] = 0;
+nromes[3] = 0;
+nromes[4] = 0;
+nromes[5] = 0;
+nromes[6] = 0;
+
+for (let index = 0; index < nromes.length; index++) {
+  nromes [index] = prompt("ingrese un numero");
+  
+}
+ 
+
+
+  for (let i = 0; i < nromes.length; i++){
+ 
+console.log (`ingrese el numero en la posicion, ${i}, es, ${nromes[i]}´);
+  }
